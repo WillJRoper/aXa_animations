c4f606ce2926860a98ed1195298cd2933b8336a4
wjr21 <wjr21@sussex.ac.uk>
2021-09-13 16:36:03 +0200

    Dark matter colours

--- PySPHviewer_scripts/dm_animate.py
+++ PySPHviewer_scripts/dm_animate.py
@@ -12,6 +12,7 @@ import sys
 from guppy import hpy; h = hpy()
 import os
 from swiftsimio import load
+import cmasher as cmr
 import unyt
 import gc
 
@@ -171,17 +172,17 @@ def single_frame(num, nframes):
           np.log10(3000 * mean_den) / vmax,
           np.log10(4000 * mean_den) / vmax)
 
-    hex_list = ["#000000", "#03045e", "#023e8a", "#0077b6",
+    hex_list = ["#000000", "#03045e", "#0077b6",
                 "#48cae4", "#caf0f8", "#ffffff"]
     float_list = [0,
-                  np.log10(0.5 * mean_den) / vmax,
                   np.log10(mean_den) / vmax,
                   np.log10(200 * mean_den) / vmax,
                   np.log10(1600 * mean_den) / vmax,
                   np.log10(1600 * mean_den) / vmax,
                   1.0]
 
-    cmap = get_continuous_cmap(hex_list, float_list=float_list)
+    # cmap = get_continuous_cmap(hex_list, float_list=float_list)
+    cmap = cmr.neutral
 
     # Get images
     rgb_output, ang_extent = getimage(cam_data, poss, masses, hsmls,
