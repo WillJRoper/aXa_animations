f0874bffa0edc8c2e88ce83923caecb97f1ab0fd
wjr21 <wjr21@sussex.ac.uk>
2021-09-13 15:55:41 +0200

    Dark matter colours

--- PySPHviewer_scripts/dm_animate.py
+++ PySPHviewer_scripts/dm_animate.py
@@ -172,9 +172,9 @@ def single_frame(num, nframes):
           np.log10(4000 * mean_den) / vmax)
 
     hex_list = ["#000000", "#03045e", "#0077b6", "#48cae4", "#caf0f8"]
-    float_list = [0, np.log10(0.5 * mean_den) / vmax,
-                  np.log10(mean_den) / vmax, 
-                  np.log10(200 * mean_den) / vmax, 1.0]
+    float_list = [0, np.log10(mean_den) / vmax,
+                  np.log10(200 * mean_den) / vmax,
+                  np.log10(1600 * mean_den) / vmax, 1.0]
 
     cmap = get_continuous_cmap(hex_list, float_list=float_list)
 
