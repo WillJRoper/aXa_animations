e9c008fe43de1cf96eeab89b5fad29273d1b5128
wjr21 <wjr21@sussex.ac.uk>
2021-09-15 12:54:46 +0200

    simplified all scripts

--- PySPHviewer_scripts/dm_animate.py
+++ PySPHviewer_scripts/dm_animate.py
@@ -65,7 +65,7 @@ def single_frame(num, nframes, res):
 
     mean_den = np.sum(masses) / boxsize ** 3
 
-    vmax, vmin = np.log10(4000 * mean_den), 3
+    vmax, vmin = np.log10(5000 * mean_den), 3
 
     print(vmax)
 
