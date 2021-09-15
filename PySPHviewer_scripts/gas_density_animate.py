2cac256093678a04ea34d45453c5a940b87fabbd
wjr21 <wjr21@sussex.ac.uk>
2021-09-15 13:53:52 +0200

    simplified all scripts

--- PySPHviewer_scripts/gas_density_animate.py
+++ PySPHviewer_scripts/gas_density_animate.py
@@ -63,7 +63,7 @@ def single_frame(num, nframes, res):
 
     mean_den = np.sum(dm_masses) / boxsize ** 3
 
-    vmax, vmin = np.log10(3000 * mean_den), 6
+    vmax, vmin = np.log10(5000 * mean_den), 6
 
     cmap = cmr.chroma
 
