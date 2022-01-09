d640d28d29ed88b566e263935d37848b19a14c38
wjr21 <wjr21@sussex.ac.uk>
2022-01-09 23:11:01 +0000

    task farm for individual simulation cells

--- PySPHviewer_scripts/dm_animate_flamingo.py
+++ PySPHviewer_scripts/dm_animate_flamingo.py
@@ -47,7 +47,7 @@ def single_frame(num, nframes, size, rank, comm):
     nparts = hdf["Header"].attrs["NumPart_Total"][1]
     pmass = hdf["/PartType1/Masses"][0] * 10 ** 10
     ncell_dimens = hdf["Cells/Meta-data"].attrs["dimension"]
-    ncells = hdf["/Cells/Meta-data"].attrs["nr_cells"]
+    ncells = hdf["/Cells/Meta-data"].attrs["nr_cells"] // 10
     cell_width = hdf["Cells/Meta-data"].attrs["size"]
     tot_mass = nparts * pmass
 
@@ -169,7 +169,8 @@ def single_frame(num, nframes, size, rank, comm):
 
                     # Get images
                     img = make_spline_img_cart(poss, res, w, h, masses, hsmls)
-                    print("Image limits:", img.min(), img.max())
+                    print("Image limits:",
+                          np.log10(img.min()), np.log10(img.max()), my_cell)
 
                     # if img.max() > 0:
                     #
