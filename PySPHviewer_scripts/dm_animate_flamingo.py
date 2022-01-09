ed21080a84da2eba036c32a6f473dcdfb3db47f7
wjr21 <wjr21@sussex.ac.uk>
2022-01-09 17:55:37 +0000

    task farm for individual simulation cells

--- PySPHviewer_scripts/dm_animate_flamingo.py
+++ PySPHviewer_scripts/dm_animate_flamingo.py
@@ -11,6 +11,7 @@ import sys
 import h5py
 from images import get_mono_image
 import cmasher as cmr
+import utilities
 import os
 import mpi4py
 from mpi4py import MPI
@@ -21,9 +22,12 @@ mpi4py.rc.recv_mprobe = False
 comm = MPI.COMM_WORLD  # get MPI communicator object
 size = comm.size  # total number of processes
 rank = comm.rank  # rank of this process
+status = MPI.Status()  # get MPI status object
 
 
 def single_frame(num, nframes, res, size, rank, comm):
+    # Define MPI message tags
+    tags = utilities.enum('READY', 'DONE', 'EXIT', 'START')
 
     snap = "0020"
 
@@ -41,16 +45,31 @@ def single_frame(num, nframes, res, size, rank, comm):
     boxsize = hdf["Header"].attrs["BoxSize"][0]
     z = hdf["Header"].attrs["Redshift"]
     nparts = hdf["Header"].attrs["NumPart_Total"][1]
-    pmass = hdf["/PartType1/Masses"][0] * 10**10
+    pmass = hdf["/PartType1/Masses"][0] * 10 ** 10
+    ncell_dimens = hdf["Cells/Meta-data"].attrs["dimension"]
+    ncells = hdf["/Cells/Meta-data"].attrs["nr_cells"]
+    cell_width = hdf["Cells/Meta-data"].attrs["size"]
     tot_mass = nparts * pmass
+
+    # Define the simulation's "resolution"
+    pix_res = hdf["/PartType1/Softenings"][0]
 
     if rank == 0:
         print("Boxsize:", boxsize)
         print("Redshift:", z)
         print("Npart:", nparts)
+        print("Number of cells:", ncells)
+        print("Cell width:", cell_width)
+        print("Cell Pixel resolution:", int(np.floor(cell_width / pix_res)))
+        print("Cell Pixel resolution (with padding):",
+              2 * int(np.floor(cell_width / pix_res)))
+        print("Full image resolution:", int(np.floor(boxsize / pix_res)))
 
     # Define centre
-    cent = np.array([boxsize / 2, boxsize / 2, boxsize / 2])
+    true_cent = np.array([boxsize / 2, boxsize / 2, boxsize / 2])
+
+    # Define the camera's position
+    cam_pos = np.array([boxsize / 2, boxsize / 2, - boxsize])
 
     # Define targets
     targets = [[0, 0, 0]]
@@ -69,91 +88,157 @@ def single_frame(num, nframes, res, size, rank, comm):
     anchors['p'] = [0, 'pass', 'pass', 'pass', 'pass', 'pass', 'pass', -360]
     anchors['zoom'] = [1., 'same', 'same', 'same', 'same', 'same', 'same',
                        'same']
-    anchors['extent'] = [boxsize, 'same', 'same', 'same', 'same', 'same', 'same',
+    anchors['extent'] = [2 * cell_width, 'same', 'same', 'same', 'same',
+                         'same', 'same',
                          'same']
 
-    # Define the camera trajectory
-    cam_data = camera_tools.get_camera_trajectory(targets, anchors)
-
-    rank_bins = np.linspace(0, nparts, size + 1, dtype=int)
-
-    poss = hdf["/PartType1/Coordinates"][rank_bins[rank]: rank_bins[rank + 1]]
-    masses = hdf["/PartType1/Masses"][rank_bins[rank]: rank_bins[rank + 1]] * 10**10
-    poss -= cent
-    poss[np.where(poss > boxsize / 2)] -= boxsize
-    poss[np.where(poss < - boxsize / 2)] += boxsize
-
-    hsmls = hdf["/PartType1/Softenings"][rank_bins[rank]: rank_bins[rank + 1]]
-
     if rank == 0:
-        print("Theoretical resolution:", boxsize / hsmls[0])
 
-    mean_den = tot_mass / boxsize ** 3
+        ncell = -1
 
-    vmax, vmin = 17, 0
+        # Master process executes code below
+        num_workers = size - 1
+        closed_workers = 0
+        while closed_workers < num_workers:
 
-    print("Norm:", vmin, "-", vmax)
+            data = comm.recv(source=MPI.ANY_SOURCE,
+                             tag=MPI.ANY_TAG,
+                             status=status)
+            source = status.Get_source()
+            tag = status.Get_tag()
 
-    cmap = cmr.eclipse
+            if tag == tags.READY:
 
-    # Get images
-    img, ang_extent = get_mono_image(cam_data, poss, masses, hsmls,
-                                     num, res)
+                ncell += 1
 
-    print(img)
+                # Worker is ready, so send it a task
+                if ncell < ncells:
 
-    norm = Normalize(vmin=vmin, vmax=vmax)
+                    comm.send(ncell, dest=source, tag=tags.START)
 
-    rgb_output = cmap(norm(img))
+                else:
 
-    i = cam_data[num]
-    extent = [0, 2 * np.tan(ang_extent[1]) * i['r'],
-              0, 2 * np.tan(ang_extent[-1]) * i['r']]
-    print("Extents:", ang_extent, extent)
+                    # There are no tasks left so terminate this process
+                    comm.send(None, dest=source, tag=tags.EXIT)
 
-    dpi = rgb_output.shape[0] / 2
-    print("DPI, Output Shape:", dpi, rgb_output.shape)
-    fig = plt.figure(figsize=(2, 2 * 1.77777777778), dpi=dpi)
-    ax = fig.add_subplot(111)
+            elif tag == tags.EXIT:
 
-    ax.imshow(rgb_output, extent=ang_extent, origin='lower')
-    ax.tick_params(axis='both', left=False, top=False, right=False,
-                   bottom=False, labelleft=False,
-                   labeltop=False, labelright=False, labelbottom=False)
+                closed_workers += 1
 
-    ax.text(0.975, 0.05, "$t=$%.1f Gyr" % cosmo.age(z).value,
-            transform=ax.transAxes, verticalalignment="top",
-            horizontalalignment='right', fontsize=1, color="w")
+    else:
 
-    ax.plot([0.05, 0.15], [0.025, 0.025], lw=0.1, color='w',
-            clip_on=False,
-            transform=ax.transAxes)
+        results = []
 
-    ax.plot([0.05, 0.05], [0.022, 0.027], lw=0.15, color='w',
-            clip_on=False,
-            transform=ax.transAxes)
-    ax.plot([0.15, 0.15], [0.022, 0.027], lw=0.15, color='w',
-            clip_on=False,
-            transform=ax.transAxes)
+        while True:
 
-    axis_to_data = ax.transAxes + ax.transData.inverted()
-    left = axis_to_data.transform((0.05, 0.075))
-    right = axis_to_data.transform((0.15, 0.075))
-    dist = extent[1] * (right[0] - left[0]) / (
-            ang_extent[1] - ang_extent[0])
+            comm.send(None, dest=0, tag=tags.READY)
+            my_cell = comm.recv(source=0, tag=MPI.ANY_TAG, status=status)
+            tag = status.Get_tag()
 
-    ax.text(0.1, 0.055, "%.2f cMpc" % dist,
-            transform=ax.transAxes, verticalalignment="top",
-            horizontalalignment='center', fontsize=1, color="w")
+            if tag == tags.START:
 
-    plt.margins(0, 0)
+                # Retrieve the offset and counts
+                my_offset = hdf["/Cells/OffsetsInFile/PartType1"][my_cell]
+                my_count = hdf["/Cells/Counts/PartType1"][my_cell]
+                my_cent = hdf["/Cells/Centres"][my_cell, :]
 
-    fig.savefig('../plots/Ani/DM/Flamingo_DM_' + frame
-                + '_' + str(rank) + '.png',
-                bbox_inches='tight',
-                pad_inches=0)
+                poss = hdf["/PartType1/Coordinates"][
+                       my_offset:my_offset + my_count, :]
+                masses = hdf["/PartType1/Masses"][
+                         my_offset:my_offset + my_count] * 10 ** 10
+                poss -= my_cent
 
-    plt.close(fig)
+                hsmls = hdf["/PartType1/Softenings"][
+                        my_offset:my_offset + my_count]
+
+                # Compute camera radial distance to cell
+                cam_sep = cam_pos - my_cent
+                cam_dist = np.sqrt(cam_sep[0] ** 2
+                                   + cam_sep[1] ** 2
+                                   + cam_sep[2] ** 2)
+
+                # Define anchors dict for camera parameters
+                anchors['r'] = ["infinity", 'same', 'same',
+                                'same', 'same', 'same',
+                                'same', 'same']
+
+                # Define the camera trajectory
+                cam_data = camera_tools.get_camera_trajectory(targets, anchors)
+
+                mean_den = tot_mass / boxsize ** 3
+
+                vmax, vmin = 8, 0
+
+                print("Norm:", vmin, "-", vmax)
+
+                cmap = cmr.eclipse
+
+                # Get images
+                img, ang_extent = get_mono_image(cam_data, poss, masses, hsmls,
+                                                 num, res)
+
+                print(img)
+
+                norm = Normalize(vmin=vmin, vmax=vmax)
+
+                rgb_output = cmap(norm(img))
+
+                i = cam_data[num]
+                extent = [0, 2 * np.tan(ang_extent[1]) * i['r'],
+                          0, 2 * np.tan(ang_extent[-1]) * i['r']]
+                print("Extents:", ang_extent, extent)
+
+                dpi = rgb_output.shape[0] / 2
+                print("DPI, Output Shape:", dpi, rgb_output.shape)
+                fig = plt.figure(figsize=(2, 2 * 1.77777777778), dpi=dpi)
+                ax = fig.add_subplot(111)
+
+                ax.imshow(rgb_output, extent=ang_extent, origin='lower')
+                ax.tick_params(axis='both', left=False, top=False, right=False,
+                               bottom=False, labelleft=False,
+                               labeltop=False, labelright=False,
+                               labelbottom=False)
+
+                ax.text(0.975, 0.05, "$t=$%.1f Gyr" % cosmo.age(z).value,
+                        transform=ax.transAxes, verticalalignment="top",
+                        horizontalalignment='right', fontsize=1, color="w")
+
+                ax.plot([0.05, 0.15], [0.025, 0.025], lw=0.1, color='w',
+                        clip_on=False,
+                        transform=ax.transAxes)
+
+                ax.plot([0.05, 0.05], [0.022, 0.027], lw=0.15, color='w',
+                        clip_on=False,
+                        transform=ax.transAxes)
+                ax.plot([0.15, 0.15], [0.022, 0.027], lw=0.15, color='w',
+                        clip_on=False,
+                        transform=ax.transAxes)
+
+                axis_to_data = ax.transAxes + ax.transData.inverted()
+                left = axis_to_data.transform((0.05, 0.075))
+                right = axis_to_data.transform((0.15, 0.075))
+                dist = extent[1] * (right[0] - left[0]) / (
+                        ang_extent[1] - ang_extent[0])
+
+                ax.text(0.1, 0.055, "%.2f cMpc" % dist,
+                        transform=ax.transAxes, verticalalignment="top",
+                        horizontalalignment='center', fontsize=1, color="w")
+
+                plt.margins(0, 0)
+
+                fig.savefig('../plots/Ani/DM/Flamingo_DM_' + frame
+                            + '_' + str(my_cell) + '.png',
+                            bbox_inches='tight',
+                            pad_inches=0)
+
+                plt.close(fig)
+
+                results.append((my_cell, my_cent, img))
+
+            elif tag == tags.EXIT:
+                break
+
+        comm.send(None, dest=0, tag=tags.EXIT)
 
     # collected_img = comm.gather(img, root=0)
     #
@@ -212,16 +297,17 @@ def single_frame(num, nframes, res, size, rank, comm):
     #
     #     plt.close(fig)
 
+
 nframes = 1000
 if int(sys.argv[2]) > 0:
     frame = "%05d" % int(sys.argv[1])
     if os.path.isfile('../plots/Ani/DM/Flamingo_DM_' + frame + '.png'):
         print("File exists")
     else:
-        res = (2160*2, 3840*2)
+        res = (2160 * 2, 3840 * 2)
         single_frame(int(sys.argv[1]), nframes=nframes, res=res,
                      size=size, rank=rank, comm=comm)
 else:
-    res = (2160*2, 3840*2)
+    res = (2160 * 2, 3840 * 2)
     single_frame(int(sys.argv[1]), nframes=nframes, res=res,
                  size=size, rank=rank, comm=comm)
--- PySPHviewer_scripts/utilities.py
+++ PySPHviewer_scripts/utilities.py
@@ -350,3 +350,10 @@ def get_star_formation_time(SFT, redshift, cosmo):
     SFz = (1/SFT) - 1.
     SFa = cosmo.age(redshift).value - cosmo.age(SFz).value
     return SFa
+
+def enum(*sequential, **named):
+    """Handy way to fake an enumerated type in Python
+    http://stackoverflow.com/questions/36932/how-can-i-represent-an-enum-in-python
+    """
+    enums = dict(zip(sequential, range(len(sequential))), **named)
+    return type('Enum', (), enums)
