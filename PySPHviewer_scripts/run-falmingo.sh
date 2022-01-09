0a3bb78ca6aed8b255b1703f649d755d2a620618
wjr21 <wjr21@sussex.ac.uk>
2022-01-09 22:57:20 +0000

    task farm for individual simulation cells

--- PySPHviewer_scripts/run-falmingo.sh
+++ PySPHviewer_scripts/run-falmingo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH --ntasks 64 # The number of cores you need...
 #SBATCH --array=1-10%1
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=1
 #SBATCH -J FLAMINGO-ANI #Give it something meaningful.
 #SBATCH -o logs/output_flamingo_ani.%J.%A.%a.out
 #SBATCH -e logs/output_flamingo_ani.%J.%A.%a.err
@@ -21,7 +21,7 @@ source activate flares-env
 i=$(($SLURM_ARRAY_TASK_ID - 1))
 
 # Run the program
-mpirun -np 64 python dm_animate_flamingo.py $i 0
+mpirun -np 128 python dm_animate_flamingo.py $i 0
 
 source deactivate
 
