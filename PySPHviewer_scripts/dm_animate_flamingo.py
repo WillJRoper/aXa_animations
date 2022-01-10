8310137d76b4d22a7331ec3c5865ea7cea86571a
wjr21 <wjr21@sussex.ac.uk>
2022-01-10 01:41:41 +0000

    task farm for individual simulation cells

--- PySPHviewer_scripts/dm_animate_flamingo.py
+++ PySPHviewer_scripts/dm_animate_flamingo.py
@@ -111,7 +111,7 @@ def single_frame(num, nframes, size, rank, comm):
                 ncell += 1
 
                 # Worker is ready, so send it a task
-                if ncell < ncells // 1000:
+                if ncell < ncells:
 
                     comm.send(ncell, dest=source, tag=tags.START)
 
