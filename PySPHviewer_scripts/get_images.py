0a537a341a5260b5b81a44a5b1483bbcb47cd165
wjr21 <wjr21@sussex.ac.uk>
2022-01-31 00:38:44 +0000

    split huge images into mosiac

--- PySPHviewer_scripts/get_images.py
+++ PySPHviewer_scripts/get_images.py
@@ -123,7 +123,7 @@ def make_spline_img_3d(part_pos, Ndim, pad_pix, ls, smooth, pix_res,
         i_high = i_low + norm_kernel.shape[1]
         j_high = j_low + norm_kernel.shape[0]
 
-        print(i_low, i_high, j_low, j_high, norm_kernel.shape)
+        print(i_low, i_high, j_low, j_high, norm_kernel.shape, pad_pix // 2)
 
         smooth_img[i_low: i_high, j_low: j_high, :] += l * norm_kernel
 
