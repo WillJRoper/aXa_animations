1ac607ddf91d5e7bdd4a209bd3981f3ed7be6914
wjr21 <wjr21@sussex.ac.uk>
2022-02-03 19:03:06 +0000

    gas temperature script

--- PySPHviewer_scripts/gas_animate_flamingo.py
+++ PySPHviewer_scripts/gas_animate_flamingo.py
@@ -58,7 +58,7 @@ def single_frame(num, nframes, size, rank, comm):
     cell_width = hdf["Cells/Meta-data"].attrs["size"]
     tot_mass = nparts * pmass
 
-    print(hdf["/PartType0/"].keys())
+    print(hdf["/PartType0/SmoothingLengths"].max())
 
     # Define the simulation's "resolution"
     soft = hdf["/PartType1/Softenings"][0]
@@ -158,7 +158,7 @@ def single_frame(num, nframes, size, rank, comm):
                      my_offset:my_offset + my_count] * 10 ** 10
             temps = hdf["PartType0/Temperatures"][
                      my_offset:my_offset + my_count]
-            hsmls = hdf["/PartType0/SmoothingLength"][
+            hsmls = hdf["/PartType0/SmoothingLengths"][
                     my_offset:my_offset + my_count]
 
             # Shift particle positions to this cell with pad region
