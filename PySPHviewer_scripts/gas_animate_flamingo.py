99dda8b391fe864e1a0ffdd9cf12ea3d20caa267
wjr21 <wjr21@sussex.ac.uk>
2022-02-03 19:13:45 +0000

    gas temperature script

--- PySPHviewer_scripts/gas_animate_flamingo.py
+++ PySPHviewer_scripts/gas_animate_flamingo.py
@@ -58,8 +58,6 @@ def single_frame(num, nframes, size, rank, comm):
     cell_width = hdf["Cells/Meta-data"].attrs["size"]
     tot_mass = nparts * pmass
 
-    print(hdf["/PartType0/SmoothingLengths"][...].max())
-
     # Define the simulation's "resolution"
     soft = hdf["/PartType1/Softenings"][0]
     pix_res = soft * mod
