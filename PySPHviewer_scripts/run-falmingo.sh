16f31a439b07ccff17bca279f3276a502afc10be
wjr21 <wjr21@sussex.ac.uk>
2022-03-05 10:30:31 +0000

    adjusting scripts for cosma8

--- PySPHviewer_scripts/run-falmingo.sh
+++ PySPHviewer_scripts/run-falmingo.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -l
 #SBATCH --ntasks 256 # The number of cores you need...
-#SBATCH --array=20-25
+# #SBATCH --array=20-25
 #SBATCH --cpus-per-task=4
 #SBATCH -J FLAMINGO-ANI #Give it something meaningful.
 #SBATCH -o logs/output_flamingo_ani.%J.%A.%a.out
 #SBATCH -e logs/output_flamingo_ani.%J.%A.%a.err
 #SBATCH -p cosma8
 #SBATCH -A dp004
-#SBATCH -t 24:00:00
+#SBATCH -t 42:00:00
 
 # Run the job from the following directory - change this to point to your own personal space on /lustre
-cd /cosma/home/dp004/dc-rope1/cosma7/Animations/codes/aXa_animations/PySPHviewer_scripts
+cd /cosma8/data/dp004/dc-rope1/aXa_animations/PySPHviewer_scripts
 
 module purge
 #load the modules used to build your program.
@@ -18,11 +18,11 @@ module load pythonconda3/4.5.4
 
 source activate flares-env
 
-i=$(($SLURM_ARRAY_TASK_ID - 1))
+# i=$(($SLURM_ARRAY_TASK_ID - 1))
 
 # Run the program
 #mpirun -np 512 python dm_animate_flamingo.py $i 0
-mpirun -np 256 python gas_animate_flamingo.py $i
+mpirun -np 256 python gas_animate_flamingo.py 20
 
 source deactivate
 
