b4dae72e36f190ae209381087e1ccf9d452a42c6
wjr21 <wjr21@sussex.ac.uk>
2022-09-19 20:32:23 +0100

    Decreasing width to previous width

--- webb_grp_imgs.py
+++ webb_grp_imgs.py
@@ -476,7 +476,7 @@ filters = ["JWST.NIRCAM." + f for f in ["F090W", "F150W", "F200W",
                                         "F277W", "F356W", "F444W"]]
 
 # Define the initial image size in Mpc
-width = 5.0
+width = 1.0
 
 # Get the conversion between arcseconds and pkpc at this redshift
 arcsec_per_kpc_proper = cosmo.arcsec_per_kpc_proper(z).value
