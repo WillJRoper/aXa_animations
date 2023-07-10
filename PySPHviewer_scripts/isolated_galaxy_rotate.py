80f849cac832f6f2b87dae61dcf607a6b49ea3c2
wjr21 <wjr21@sussex.ac.uk>
2023-07-10 16:56:52 +0100

    Adjusting norm

--- PySPHviewer_scripts/isolated_galaxy_rotate.py
+++ PySPHviewer_scripts/isolated_galaxy_rotate.py
@@ -64,7 +64,7 @@ def single_frame(num, nframes, res):
 
     mean_den = cosmo.Ob(0) * cosmo.critical_density(0)
 
-    vmax, vmin = 8.5, 0
+    vmax, vmin = 8., 0
 
     print("Norm:", vmin, vmax)
 
